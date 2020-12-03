9be11c4b71434531e0afb6200ebe96179abc4a31
lipengzhou <lpzmail@163.com>
2020-12-03 17:49:49 +0800

    add asyncData

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _4c6d1732 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _0682e08e = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -23,7 +23,7 @@ export const routerOptions = {
 
   routes: [{
     path: "/",
-    component: _4c6d1732,
+    component: _0682e08e,
     name: "index"
   }],
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <h1>Hello World!</h1>
+    <ul>
+      <li
+        v-for="post in posts"
+        :key="post.id"
+      >{{ post.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  async asyncData () {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return {
+      posts: data
+    }
+  }
 }
 </script>
